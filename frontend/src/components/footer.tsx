3c44b2698089f66b67824a93b6c0f5b2c34731d0
Kaushik Sarkar <akaushikarkar@gmail.com>
2025-01-30 18:31:55 +0530

    Completed footer

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,51 +1,70 @@
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-lg shadow-sm dark:bg-gray-900 m-4">
+    <footer className="bg-white rounded-lg shadow-[0px_-5px_10px_0_rgb(0_0_0_/_0.05)]  dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="https://flowbite.com/"
-            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+          <a href="/" className="flex items-center justify-center mb-4 sm:mb-0">
+            <img src="/QGCico.ico" className="h-8" alt="QUMS Logo" />
+            <span className="self-center text-2xl mx-2 font-bold font-josefins whitespace-nowrap dark:text-white">
+              QUMS*
             </span>
           </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center justify-center my-6 md:my-0 mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
+              <a
+                href="https://github.com/24kaushik/qums?tab=readme-ov-file#quantum-university-management-system-qums"
+                target="_blank"
+                className="hover:underline me-4 md:me-6"
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline me-4 md:me-6">
+              <a
+                href="https://github.com/24kaushik/qums?tab=MIT-1-ov-file"
+                className="hover:underline me-4 md:me-6"
+                target="_blank"
+              >
                 Licensing
               </a>
             </li>
             <li>
-              <a href="#" className="hover:underline">
-                Contact
+              <a
+                href="https://github.com/24kaushik/qums"
+                className="hover:underline me-4 md:me-6"
+                target="_blank"
+              >
+                Source Code
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://kaushiksarkar.me/"
+                target="_blank"
+                className="hover:underline"
+              >
+                Developer
               </a>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            Flowbite™
-          </a>
-          . All Rights Reserved.
+        <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
+          *This is a custom made ERP (frontend) and is not affiliated with
+          Quantum University.
+          <div className="my-1">
+            100% safe and secure. Open source on{" "}
+            <a
+              href="https://github.com/24kaushik/qums"
+              className="text-blue-600 underline"
+            >
+              Github
+            </a>
+            .
+          </div>
+        </span>
+        <span className="block text-sm my-3 text-gray-500 text-center dark:text-gray-400">
+          © {new Date().getFullYear()} QUMS* . All Rights Reserved.
         </span>
       </div>
     </footer>
