e3397f1e2656169a6ebbed644a3279719f57fb87
Kaushik Sarkar <akaushikarkar@gmail.com>
2025-02-09 18:40:57 +0530

    Remove unused import from LoginForm component (accidentally added)

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -4,7 +4,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { get } from "http";
 
 export function LoginForm({
   className,
